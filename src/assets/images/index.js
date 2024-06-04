b8b621dbd99fde7b5c79b637df8a9ed260bdd37a
Nadun Kawishika <hnkmalwenna@gmail.com>
2024-06-04 11:51:39 +0530

    Initialize NextJS Project

--- src/assets/images/index.js
+++
@@ -1,7 +0,0 @@
-import android_apps from "./andoid-apps.svg"
-import full_stack from "./full-stack.svg"
-
-export {
-    android_apps,
-    full_stack
-}
--- src/sections/index.jsx
+++
@@ -1,7 +0,0 @@
-import Footer from './Footer';
-import About from './About';
-import Nav from "../components/Nav";
-import Services from "./Services";
-import Contact from "./Contact.jsx"
-
-export {Footer, About, Nav, Services, Contact}
