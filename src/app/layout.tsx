2acc4f39d6b30af1077aaa793c746c1fc4e0a883
Nadun Kawishika <hnkmalwenna@gmail.com>
2024-06-05 23:00:20 +0530

    add Ico logo and remove unwanted node modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+        <head>
+            <link rel="icon" href="/logo.ico"/>
+        </head>
         <body className={inter.className}>
         <ThemeProvider
             attribute="class"
