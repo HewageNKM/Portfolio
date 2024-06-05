cb1fa1ac2453bc9f71de77fe014686dc6c2f9dc4
Nadun Kawishika <hnkmalwenna@gmail.com>
2024-06-05 18:24:56 +0530

    Complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,13 +6,13 @@ import {FaLocationArrow} from "react-icons/fa6";
 
 const Hero = () => {
     return (
-        <div className="pb-20 pt-36">
+        <div className="pb-20 pt-36" id="hero">
             <div>
                 <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
                 <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple"/>
                 <Spotlight className="-top-28 left-80 h-[80vh] w-[50vw]" fill="blue"/>
             </div>
-            <div className="h-screen absolute top-0 left-0 w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.03] flex items-center justify-center">
+            <div className="h-screen absolute top-0 left-0 w-full dark:bg-black-100 bg-white  dark:bg-dot-white/[0.3] bg-dot-black/[0.3] flex items-center justify-center">
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
             </div>
             <div className="flex justify-center relative my-20 z-10">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 import Hero from "../../components/Hero";
 import {NavBar} from "../../components/ui/NavBar";
-import {FaHome} from "react-icons/fa";
 import {Grid} from "../../components/Grid";
+import RecentProjects from "../../components/RecentProjects";
+import {navItems} from "../../data";
+import Footer from "../../components/Footer";
 
 export default function Home() {
     return (
         <main
             className="relative bg-black-100 flex flex-col justify-center items-center sm:px-10 px-5 mx-auto overflow-hidden">
             <div className="max-w-7xl w-full">
-                <NavBar navItems={[{name: 'Home', link: '/', icon: <FaHome/>}]}/>
+                <NavBar navItems={navItems}/>
                 <Hero/>
-                <Grid />
+                <Grid/>
+                <RecentProjects/>
+                <Footer/>
             </div>
         </main>
     );
