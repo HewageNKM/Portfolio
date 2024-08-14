b1a4d1d578aa688418ae14e97e7a683b824f7ef3
Nadun Kawshika <107237482+HewageNKM@users.noreply.github.com>
2024-08-14 16:38:41 +0530

    new home structure complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,23 +6,7 @@ import {socialMedia} from "../data";
 
 const Footer = () => {
     return (
-        <footer id="contact" className="w-full h-full pt-20 pb-5">
-            <div className='w-full absolute left-0 bottom-0 md:-bottom-96 min-h-96'>
-                <img src="/footer-grid.svg" alt="grid" className='opacity-50'/>
-            </div>
-            <div className="flex flex-col items-center">
-                <h1 className="heading lg:max-w-[40vw] my-5">
-                    Wanna Reach <span className="text-purple">Out?</span>
-                </h1>
-                <div className="mb-3 text-base md:text-xl font-light lg:text-xl">
-                    Let&apos;s Build Your
-                    <FlipWords words={["Beautiful","Modern","PowerFull","Cute"]} duration={2000}/>
-                    Idea Together
-                </div>
-                <a href="mailto:kawishikam@gmail.com">
-                    <MagicButton title="Let's hit" icon={<FaLocationArrow/>} position="right"/>
-                </a>
-            </div>
+        <footer className="w-full mt-20 fixed bottom-0 left-0 px-20">
             <div className="flex my-5 md:flex-row flex-col justify-center gap-3 md:justify-between items-center">
                 <p className="font-light md:font-normal font-base">Copyright © {new Date().getFullYear()} Nadun</p>
                 <div className="flex items-center md:gap-3 gap-6">
