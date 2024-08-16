989dc2529cefb6f366b593f9aea7f7270ae08ccc
Nadun Kawishika <107237482+HewageNKM@users.noreply.github.com>
2024-08-17 01:56:27 +0530

    Remove home page border

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/ui/Footer";
 import {motion} from "framer-motion";
 export default function Home() {
     return (
-        <motion.main initial={{opacity:0}} animate={{opacity:1}} className="relative border rounded-lg border-gray-900 min-h-[97vh] m-2">
+        <motion.main initial={{opacity:0}} animate={{opacity:1}} className="relative rounded-lg min-h-screen m-2">
             <Header/>
             <Hero/>
             <Footer/>
