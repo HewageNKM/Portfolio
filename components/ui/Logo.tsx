8db7a3c66b49afc17d7fc83f678954e116ac95d9
Nadun Kawishika <107237482+HewageNKM@users.noreply.github.com>
2024-08-19 09:26:45 +0530

    Change Logo

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -6,10 +6,7 @@ const Logo = () => {
     return (
         <div className="bg-gray-900 lg:hover:scale-110 transition-all duration-500 w-fit p-1 md:p-2 absolute top-5 left-5">
             <Link href="/">
-                <h1 className="md:text-xl text-lg text-center font-bold text-white tracking-wider">H</h1>
-                <h1 className="md:text-xl text-lg text-center font-bold text-white tracking-wider">N</h1>
-                <h1 className="md:text-xl text-lg text-center font-bold text-white tracking-wider">K</h1>
-                <h1 className="md:text-xl text-lg text-center font-bold text-white tracking-wider">M</h1>
+                <h1 className="md:text-6xl text-4xl text-center font-bold text-white tracking-wider">M</h1>
             </Link>
         </div>
     );
