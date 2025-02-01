6f090f15421681dafec4df292eb6ec6921df8f70
Ethan Blake <107237482+EthanBlake00@users.noreply.github.com>
2025-02-01 21:41:06 +0530

    Clean and reset

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-"use client"
-import {motion} from "framer-motion";
-import dynamic from "next/dynamic";
-const Header = dynamic(()=>import("@/components/Header"),{ssr:false});
-const Footer = dynamic(()=>import("@/components/ui/Footer"),{ssr:false});
-const Hero = dynamic(()=>import("@/components/Hero"),{ssr:false});
-
 export default function Home() {
     return (
-        <motion.main initial={{opacity:0}} animate={{opacity:1}} className="relative flex flex-col justify-between rounded-lg min-h-screen px-8 py-4">
-            <Header/>
-            <Hero/>
-            <Footer/>
-        </motion.main>
+        <main>
+            <h1>Hello World</h1>
+        </main>
     );
 }
--- assets/index.ts
+++
@@ -1,5 +0,0 @@
-import git from "./git.svg";
-import x from "./x.svg";
-import linkedin from "./link.svg";
-
-export { git, x, linkedin };
