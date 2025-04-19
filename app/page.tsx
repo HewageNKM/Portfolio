f2f5d1cb1ec2286912ad3c691cd15bac41884f63
Ethan Blake <107237482+EthanBlake00@users.noreply.github.com>
2025-04-19 20:19:55 +0530

    Remove all

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-    return (
-        <main className={"flex flex-col items-center justify-center h-screen"}>
-            <h1 className={"text-4xl font-bold text-center"}>404 - Page Not Found</h1>
-            <p className={"text-lg text-center"}>The page you are looking for does not exist.</p>
-        </main>
-    );
-}
