9566998459c97909d378c392722b29a36103b5d2
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2025-12-09 11:43:36 +0530

    Update Services grid layout to display 4 columns on large screens.

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -62,7 +62,7 @@ export default function Services() {
 
       {/* Services Grid */}
       <motion.ul
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4"
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-4"
         variants={containerVariants}
         initial="hidden"
         animate="visible"
